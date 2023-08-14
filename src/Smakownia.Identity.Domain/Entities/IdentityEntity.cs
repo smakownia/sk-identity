74b9ad9e4ea7dc544804e9439fcc6b02c253f674
aruzo <dawid12391111@gmail.com>
2023-08-14 16:46:02 +0200

    add role based authorization

--- src/Smakownia.Identity.Domain/Entities/IdentityEntity.cs
+++ src/Smakownia.Identity.Domain/Entities/IdentityEntity.cs
@@ -1,10 +1,13 @@
-﻿namespace Smakownia.Identity.Domain.Entities;
+﻿using Smakownia.Identity.Domain.Enums;
+
+namespace Smakownia.Identity.Domain.Entities;
 
 public class IdentityEntity
 {
     public IdentityEntity(string email, string password)
     {
         Id = Guid.NewGuid();
+        Role = IdentityRole.Customer;
         Email = email;
         Password = password;
     }
@@ -12,6 +15,7 @@ public class IdentityEntity
     protected IdentityEntity() { }
 
     public Guid Id { get; private set; }
+    public string Role { get; private set; }
     public string Email { get; private set; }
     public string Password { get; private set; }
 }
