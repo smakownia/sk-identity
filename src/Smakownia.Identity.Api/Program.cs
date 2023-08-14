313b7623bca87e19e53c859fd3a6989c7b355a30
aruzo <dawid12391111@gmail.com>
2023-08-14 15:35:44 +0200

    remove unused middleware

--- src/Smakownia.Identity.Api/Program.cs
+++ src/Smakownia.Identity.Api/Program.cs
@@ -1,28 +1,8 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Smakownia.Identity.Api.Middlewares;
 using Smakownia.Identity.Application;
 using Smakownia.Identity.Infrastructure;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAuthentication(opt =>
-{
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
-        };
-    });
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
@@ -42,9 +22,6 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapControllers();
 
